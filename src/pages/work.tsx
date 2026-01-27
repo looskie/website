85b1d700322ac5fc0403e7413a01001d51f2fbdb
Cody Miller <hello@looskie.com>
2026-01-27 10:04:06 -0500

    SIDE BY SIDE BABY

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -16,70 +16,76 @@ export default function Work() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="title__wrapper">
-        <AnimatedText element="h1" text="Work" />
-      </div>
-      <div className="flex flex-col gap-[100px]">
-        {WORK.map((work, index) => (
-          <div
-            key={work.company}
-            className="relative flex flex-col max-w-[700px]"
-          >
-            <span className="absolute -left-[45px] -top-[30px] text-5xl text-primary-100 opacity-10">
-              {index.toString().padStart(2, "0")}
-            </span>
-            <h2 className="text-5xl font-bold text-primary-100 mb-3">
-              {work.company}
-              {work.subtitle ? (
-                <span className="text-lg font-normal text-primary-300 ml-2">
-                  ({work.subtitle})
-                </span>
-              ) : null}
-              {work.dates ? (
-                <span className="text-lg font-normal text-primary-400 ml-3">
-                  {work.dates}
-                </span>
-              ) : null}
-            </h2>
-            <p className="text-xl font-light text-primary-200 mb-3">
-              <span className="font-semibold text-primary-400">
-                {work.position} —{" "}
-              </span>
-              {work.description}
-            </p>
-            <a href={work.link} target="_blank" rel="noreferrer">
-              visit website
-            </a>
+      <div className="flex flex-col 2xl:flex-row 2xl:gap-[100px]">
+        <div className="flex-1">
+          <div className="title__wrapper">
+            <AnimatedText element="h1" text="Work" />
           </div>
-        ))}
-      </div>
+          <div className="flex flex-col gap-[100px]">
+            {WORK.map((work, index) => (
+              <div
+                key={work.company}
+                className="relative flex flex-col max-w-[700px]"
+              >
+                <span className="absolute -left-[45px] -top-[30px] text-5xl text-primary-100 opacity-10">
+                  {index.toString().padStart(2, "0")}
+                </span>
+                <h2 className="text-5xl font-bold text-primary-100 mb-3">
+                  {work.company}
+                  {work.subtitle ? (
+                    <span className="text-lg font-normal text-primary-300 ml-2">
+                      ({work.subtitle})
+                    </span>
+                  ) : null}
+                  {work.dates ? (
+                    <span className="text-lg font-normal text-primary-400 ml-3">
+                      {work.dates}
+                    </span>
+                  ) : null}
+                </h2>
+                <p className="text-xl font-light text-primary-200 mb-3">
+                  <span className="font-semibold text-primary-400">
+                    {work.position} —{" "}
+                  </span>
+                  {work.description}
+                </p>
+                <a href={work.link} target="_blank" rel="noreferrer">
+                  visit website
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
 
-      <div className="title__wrapper mt-[100px]">
-        <AnimatedText element="h1" text="OSS" />
-      </div>
-      <div className="flex flex-col gap-[100px]">
-        {OSS.map((project, index) => (
-          <div
-            key={project.name}
-            className="relative flex flex-col max-w-[700px]"
-          >
-            <span className="absolute -left-[45px] -top-[30px] text-5xl text-primary-100 opacity-10">
-              {index.toString().padStart(2, "0")}
-            </span>
-            <h2 className="text-5xl font-bold text-primary-100 mb-3">
-              {project.name}
-            </h2>
-            <p className="text-xl font-light text-primary-200 mb-3">
-              <span className="font-semibold text-primary-400">
-                {project.role} —{" "}
-              </span>
-              {project.description}
-            </p>
-            <a href={project.link} target="_blank" rel="noreferrer">
-              view project
-            </a>
+        <div className="flex-1">
+          <div className="title__wrapper mt-[100px] 2xl:mt-0">
+            <AnimatedText element="h1" text="OSS" />
           </div>
-        ))}
+          <div className="flex flex-col gap-[100px]">
+            {OSS.map((project, index) => (
+              <div
+                key={project.name}
+                className="relative flex flex-col max-w-[700px]"
+              >
+                <span className="absolute -left-[45px] -top-[30px] text-5xl text-primary-100 opacity-10">
+                  {index.toString().padStart(2, "0")}
+                </span>
+                <h2 className="text-5xl font-bold text-primary-100 mb-3">
+                  {project.name}
+                </h2>
+                <p className="text-xl font-light text-primary-200 mb-3">
+                  <span className="font-semibold text-primary-400">
+                    {project.role} —{" "}
+                  </span>
+                  {project.description}
+                </p>
+                <a href={project.link} target="_blank" rel="noreferrer">
+                  view project
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </AnimatedPage>
   );
