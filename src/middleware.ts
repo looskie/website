ed1e8b4812a35ab7d8bfcbdb19886e22d8821aba
Cody Miller <hello@looskie.com>
2026-01-26 21:03:00 -0500

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,7 +13,10 @@ export const config = {
 };
 
 export default async function middleware(request: NextRequest) {
-  const ip = request.ip ?? "127.0.0.1";
+  const ip =
+    request.headers.get("x-forwarded-for")?.split(",")[0]?.trim() ??
+    request.headers.get("x-real-ip") ??
+    "127.0.0.1";
   const { success } = await ratelimit.limit(ip);
 
   return success
