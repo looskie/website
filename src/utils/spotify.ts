6f7286f4d96b79528468bc15288d3a5c3288442e
Cody Miller <hello@looskie.com>
2026-03-03 21:19:54 -0500

    https

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -2,5 +2,5 @@ import { createKaitoHTTPClient } from "@kaito-http/client";
 import type { API } from "../../spotify-worker/src";
 
 export const spotifyAPI = createKaitoHTTPClient<API>({
-  base: "http://spotify.cody.bio",
+  base: "https://spotify.cody.bio",
 });
